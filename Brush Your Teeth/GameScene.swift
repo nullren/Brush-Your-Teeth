b393480c0173a00a4b0ec974a7ab79f6e8c44219
Renning Bruns <ren@renmail.net>
2023-05-07 21:32:10 -0700

    added a background and a sprite for the child whose teeth we must brush

--- Brush Your Teeth/GameScene.swift
+++ Brush Your Teeth/GameScene.swift
@@ -8,194 +8,41 @@
 import SpriteKit
 import GameplayKit
 
-struct PhysicsCategory {
-    static let none: UInt32 = 0
-    static let all: UInt32 = UInt32.max
-    static let toothbrush: UInt32 = 0b1
-    static let germ: UInt32 = 0b10
-}
-
 class GameScene: SKScene {
+    var spriteNode: SKSpriteNode?
     
-    var entities = [GKEntity]()
-    var graphs = [String : GKGraph]()
-    
-    private var lastUpdateTime : TimeInterval = 0
-    private var spinnyNode : SKShapeNode?
-    private var toothbrush : SKNode?
-    
-    override func sceneDidLoad() {
-        self.lastUpdateTime = 0
+    override func didMove(to view: SKView) {
+        let background = SKSpriteNode(imageNamed: "yoko-honda")
+        background.position = CGPoint(x: -10, y: -275)
+        background.anchorPoint = CGPoint(x: 0, y: 0)
+        addChild(background)
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            spinnyNode.alpha = 0.3
-            spinnyNode.zPosition = 2
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
+        let screenSize = UIScreen.main.bounds.size
+        let texture = SKTexture(imageNamed: "closed")
+        let aspectRatio = texture.size().width / texture.size().height
+        let proportion: CGFloat = 0.4
+        let maxWidth = screenSize.width * proportion
+        let maxHeight = screenSize.height * proportion
+        var spriteSize = CGSize(width: maxWidth, height: maxHeight)
+        if aspectRatio > 1 {
+            spriteSize.height = spriteSize.width / aspectRatio
+        } else {
+            spriteSize.width = spriteSize.height * aspectRatio
         }
+
+        // Create your sprite node with the calculated size and position it at the center of the screen
+        self.spriteNode = SKSpriteNode(texture: texture, size: spriteSize)
+        self.spriteNode!.position = CGPoint(x: screenSize.width / 2, y: screenSize.height / 2)
         
-        run(SKAction.repeatForever(
-            SKAction.sequence([
-                SKAction.run(self.addGerm),
-                SKAction.wait(forDuration: 1.0)
-            ])
-        ))
-        
-        physicsWorld.gravity = .zero
-        physicsWorld.contactDelegate = self
-        
-    }
-    
-    func random(min: CGFloat, max: CGFloat) -> CGFloat {
-      return CGFloat(Float(arc4random()) / 0xFFFFFFFF) * (max - min) + min
-    }
-    
-    func addGerm() {
-        let germ = SKLabelNode()
-        germ.text = "🦠"
-        germ.fontSize = 20
-        germ.horizontalAlignmentMode = .center
-        germ.verticalAlignmentMode = .center
-        
-        let x = size.width/2 + 20
-        let y = self.random(min: -(size.height/2 - 10), max: size.height/2 - 10)
-        let speed = self.random(min: 2.0, max: 4.0)
-        germ.position = CGPoint(x: -x, y: y)
-        germ.zPosition = 5
-        self.addChild(germ)
-        
-        germ.run(SKAction.sequence([
-            SKAction.move(to: CGPoint(x: x, y: y), duration: speed),
-            SKAction.removeFromParent()
-        ]))
-        
-        germ.physicsBody = SKPhysicsBody(circleOfRadius: germ.fontSize)
-        germ.physicsBody?.isDynamic = true // 2
-        germ.physicsBody?.categoryBitMask = PhysicsCategory.germ // 3
-        germ.physicsBody?.contactTestBitMask = PhysicsCategory.toothbrush // 4
-        germ.physicsBody?.collisionBitMask = PhysicsCategory.none // 5
-    }
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+        // Add your sprite node to your scene
+        self.addChild(self.spriteNode!)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let touch = touches.first {
-            let location = touch.location(in: self)
-            
-            let touchedNodes = self.nodes(at: location)
-            for node in touchedNodes.reversed() {
-                if node.name == "toothbrush" {
-                    node.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-                    self.toothbrush = node
-                    self.toothbrush?.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 10, height: 250))
-                    self.toothbrush?.physicsBody?.isDynamic = true
-                    self.toothbrush?.physicsBody?.categoryBitMask = PhysicsCategory.toothbrush
-                    self.toothbrush?.physicsBody?.contactTestBitMask = PhysicsCategory.germ
-                    self.toothbrush?.physicsBody?.collisionBitMask = PhysicsCategory.none
-                    self.toothbrush?.physicsBody?.usesPreciseCollisionDetection = true
-                }
-            }
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-        if let touch = touches.first, let node = self.toothbrush {
-            let touchLocation = touch.location(in: self)
-            node.position = touchLocation
-        }
+        self.spriteNode?.texture = SKTexture(imageNamed: "open")
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-        self.toothbrush?.physicsBody = nil
-        self.toothbrush = nil
+        self.spriteNode?.texture = SKTexture(imageNamed: "closed")
     }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-        self.toothbrush?.physicsBody = nil
-        self.toothbrush = nil
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-        
-        // Initialize _lastUpdateTime if it has not already been
-        if (self.lastUpdateTime == 0) {
-            self.lastUpdateTime = currentTime
-        }
-        
-        // Calculate time since last update
-        let dt = currentTime - self.lastUpdateTime
-        
-        // Update entities
-        for entity in self.entities {
-            entity.update(deltaTime: dt)
-        }
-        
-        self.lastUpdateTime = currentTime
-    }
-    func toothbrushDidCollideWithGerm(toothbrush: SKLabelNode, germ: SKLabelNode) {
-        print("Hit")
-        germ.removeFromParent()
-    }
-}
-
-extension GameScene: SKPhysicsContactDelegate {
-    func didBegin(_ contact: SKPhysicsContact) {
-      // 1
-      var firstBody: SKPhysicsBody
-      var secondBody: SKPhysicsBody
-      if contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask {
-        firstBody = contact.bodyA
-        secondBody = contact.bodyB
-      } else {
-        firstBody = contact.bodyB
-        secondBody = contact.bodyA
-      }
-        print(firstBody, secondBody)
-     
-      // 2
-      if ((firstBody.categoryBitMask & PhysicsCategory.toothbrush != 0) &&
-          (secondBody.categoryBitMask & PhysicsCategory.germ != 0)) {
-        if let germ = secondBody.node as? SKLabelNode,
-          let toothbrush = firstBody.node as? SKLabelNode {
-            toothbrushDidCollideWithGerm(toothbrush: toothbrush, germ: germ)
-        }
-      }
-    }
-
 }
--- Brush Your Teeth/GameViewController.swift
+++ Brush Your Teeth/GameViewController.swift
@@ -13,40 +13,26 @@ class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Load 'GameScene.sks' as a GKScene. This provides gameplay related content
-        // including entities and graphs.
-        if let scene = GKScene(fileNamed: "GameScene") {
+        if let view = self.view as! SKView? {
+            let scene = GameScene(size: view.bounds.size)
+            scene.scaleMode = .aspectFill
+            scene.backgroundColor = UIColor(red: 105/255, green: 157/255, blue: 181/255, alpha: 1.0)
+            view.presentScene(scene)
+            view.ignoresSiblingOrder = false
+            view.showsPhysics = false
+            view.showsFPS = true
+            view.showsNodeCount = true
             
-            // Get the SKScene from the loaded GKScene
-            if let sceneNode = scene.rootNode as! GameScene? {
-                
-                // Copy gameplay related content over to the scene
-                sceneNode.entities = scene.entities
-                sceneNode.graphs = scene.graphs
-                
-                // Set the scale mode to scale to fit the window
-                sceneNode.scaleMode = .aspectFill
-                
-                // Present the scene
-                if let view = self.view as! SKView? {
-                    view.presentScene(sceneNode)
-                    
-                    view.ignoresSiblingOrder = true
-                    
-                    view.showsFPS = true
-                    view.showsNodeCount = true
-                }
-            }
+            print("scene.size: \(scene.size)")
         }
+    }
+    
+    override var shouldAutorotate: Bool {
+        return true
     }
 
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
+        return .landscape
     }
 
     override var prefersStatusBarHidden: Bool {
