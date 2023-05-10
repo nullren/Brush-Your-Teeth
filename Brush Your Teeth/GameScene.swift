b4bda2ae592f097cf327099fa51f788a0817e714
Renning Bruns <ren@renmail.net>
2023-05-09 21:41:54 -0700

    closer to having something that can be fun

--- Brush Your Teeth/GameScene.swift
+++ Brush Your Teeth/GameScene.swift
@@ -31,18 +31,51 @@ class GameScene: SKScene {
         }
 
         // Create your sprite node with the calculated size and position it at the center of the screen
-        self.spriteNode = SKSpriteNode(texture: texture, size: spriteSize)
-        self.spriteNode!.position = CGPoint(x: screenSize.width / 2, y: screenSize.height / 2)
+        spriteNode = SKSpriteNode(texture: texture, size: spriteSize)
+        spriteNode!.position = CGPoint(x: screenSize.width / 2, y: screenSize.height / 2)
         
+        addGerm(at: CGPoint(x: 10, y: -30))
+        addGerm(at: CGPoint(x: -100, y: 0))
+        addGerm(at: CGPoint(x: 50, y: -100))
+        addGerm(at: CGPoint(x: -75, y: -95))
+
         // Add your sprite node to your scene
-        self.addChild(self.spriteNode!)
+        addChild(spriteNode!)
+    }
+    
+    func addGerm(at pos: CGPoint) {
+        // add a germ sprite relative to the spriteNode
+        let germSprite = SKLabelNode(text: "🦠")
+        germSprite.name = "germ"
+        germSprite.position = pos
+        germSprite.isHidden = true
+        spriteNode?.addChild(germSprite)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        self.spriteNode?.texture = SKTexture(imageNamed: "open")
+        spriteNode?.texture = SKTexture(imageNamed: "open")
+        spriteNode?.enumerateChildNodes(withName: "germ") { child, _ in
+            // Access each child sprite here and perform an action
+            child.isHidden = false
+        }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        let touchLocation = touch.location(in: self)
+        let nodes = self.nodes(at: touchLocation)
+        for node in nodes {
+            if node.name == "germ" {
+                node.removeFromParent()
+            }
+        }
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        self.spriteNode?.texture = SKTexture(imageNamed: "closed")
+        spriteNode?.texture = SKTexture(imageNamed: "closed")
+        spriteNode?.enumerateChildNodes(withName: "germ") { child, _ in
+            // Access each child sprite here and perform an action
+            child.isHidden = true
+        }
     }
 }
